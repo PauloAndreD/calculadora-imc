3b977e2f1a3bed23760e8511cd4b457a5db19a62
PauloAndreD <127879204+PauloAndreD@users.noreply.github.com>
2023-05-25 23:47:16 -0300

    height

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", function (event) {
   const weight = document.getElementById("weight").value;
   const height = document.getElementById("height").value;
 
-  const bmi = (weight / (height * weight)).toFixed(2);
+  const bmi = (weight / (height * height)).toFixed(2);
 
   const value = document.getElementById("value");
   let descrition = "";
